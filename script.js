06d100ebf8100a30c9bfd1f67e136b69fe217d48
Felix <felixmcwong@gmail.com>
2025-07-22 09:56:57 +1200

    Added theme and font

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    document.body.classList.add('dark-theme');
+const themeToggleBtn = document.getElementById('theme-toggle');
+var currentTheme = localStorage.getItem('theme') || 'light-theme';
+const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+if (darkThemeMq.matches && !localStorage.getItem('theme')) {
+    currentTheme = 'dark-theme';
 }
-else {
-    document.body.classList.add('light-theme');
+else if (!darkThemeMq.matches && !localStorage.getItem('theme')) {
+    currentTheme = 'light-theme';
 }
+document.body.classList.add(currentTheme);
+
+themeToggleBtn.addEventListener('click', () => {
+    const newTheme = document.body.classList.contains('light-theme') ? 'dark-theme' : 'light-theme';
+    document.body.classList.remove('light-theme', 'dark-theme');
+    document.body.classList.add(newTheme);
+    localStorage.setItem('theme', newTheme);
+});
