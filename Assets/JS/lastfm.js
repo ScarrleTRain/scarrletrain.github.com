ee2f31597703879a68641797494976bbdb1917f3
Felix <felixmcwong+Github@gmail.com>
2025-07-29 19:46:30 +1200

    Fixed lastfm format

--- Assets/JS/lastfm.js
+++ Assets/JS/lastfm.js
@@ -10,30 +10,30 @@ const coverArtSizes = Object.freeze({
 })
 
 fetch(url)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(json) {
-    console.log(json["track"]);
-    const track = json["track"];
-      
-    const playTitle = document.getElementById("play-state");
-    const art = document.getElementById("track-art");
-    const name = document.getElementById("track-name");
-    const artist = document.getElementById("track-artist");
-    const album = document.getElementById("track-album");
+    .then(function(response) {
+      return response.json();
+    })  
+    .then(function(json) {
+        //console.log(json["track"]);
+        const track = json["track"];
+        
+        const playTitle = document.getElementById("play-state");
+        const art = document.getElementById("track-art");
+        const name = document.getElementById("track-name");
+        const artist = document.getElementById("track-artist");
+        const album = document.getElementById("track-album");
 
-    if (track["@attr"] ) {
-        if (track["@attr"]["nowplaying"]) {
-            playTitle.innerText = "Now Playing";
+        if (track["@attr"] ) {
+            if (track["@attr"]["nowplaying"]) {
+                playTitle.innerText = "Now Playing";
+            }
+        } 
+        else {
+            playTitle.innerText = "Last Played";
         }
-    } 
-    else {
-        playTitle.innerText = "Last Played";
-    }
 
-    art.src = track["image"][coverArtSizes.large][t];
-    name.innerText = track["name"];
-    artist.innerText = track["artist"][t];
-    album.innerText = track["album"][t];
-  });
+        art.src = track["image"][coverArtSizes.large][t];
+        name.innerText = track["name"];
+        artist.innerText = track["artist"][t];
+      album.innerText = track["album"][t];
+    });
