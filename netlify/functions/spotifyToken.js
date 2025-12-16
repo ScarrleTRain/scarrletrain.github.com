436b11333171da16bbc442b95c92738c6b3fadc0
Felix <felixmcwong+Github@gmail.com>
2025-12-16 16:19:31 +1300

    spotify unrelated go away

---
+++ netlify/functions/spotifyToken.js
@@ -0,0 +1,27 @@
+import fetch from "node-fetch";
+
+export async function handler(event) {
+    const params = new URLSearchParams({
+        grant_type: "client_credentials"
+    });
+
+    const auth = Buffer.from(
+        process.env.SPOTIFY_CLIENT_ID + ":" + process.env.SPOTIFY_CLIENT_SECRET
+    ).toString("base64");
+
+    const res = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: {
+            Authorization: `Basic ${auth}`,
+            "Content-Type": "application/x-www-form-urlencoded",
+            },
+        body: params,
+    });
+
+    const data = await res.json();
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(data),
+    };
+}
